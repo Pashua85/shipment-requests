f86717c776b7c2e334bfaf55b3adb0172fd8b25b
Pashua85 <pavellashuk85@gmail.com>
2020-10-28 14:19:02 +0500

    add json-server with mock data

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {IRequest} from '../../types';
 
 const App: React.FC = () => {
+  const [requests, setRequests] = useState<IRequest[]>([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:1234/requests`)
+        .then(res => res.json())
+        .then(result => {
+          setRequests(result);
+        })
+  }, [])
+
   return (
     <h1>Заявки</h1>
   )
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface IRequest {
+  requestNumber: number,
+  id: string,
+  applyTime: number,
+  clientFirm: string,
+  transporter: string,
+  transporterPhone: string,
+  comments: string,
+  codeATI: string
+}
