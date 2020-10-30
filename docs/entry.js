5ce342d3058b9d25f0f7e3aa7bc8b402d35ddf21
Pashua85 <pavellashuk85@gmail.com>
2020-10-31 00:17:04 +0500

    return the docs

---
+++ docs/entry.js
@@ -0,0 +1,47 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/components/app/app.tsx';
+reactComponents['App'] = Component0;
+
+import Component1 from '../src/components/create-request-form/create-request-form.tsx';
+reactComponents['CreateRequestForm'] = Component1;
+
+import Component2 from '../src/components/header/header.tsx';
+reactComponents['Header'] = Component2;
+
+import Component3 from '../src/components/input-group/input-group.tsx';
+reactComponents['InputGroup'] = Component3;
+
+import Component4 from '../src/components/panel/panel.tsx';
+reactComponents['Panel'] = Component4;
+
+import Component5 from '../src/components/request-form-view/request-form-view.tsx';
+reactComponents['RequestFormView'] = Component5;
+
+import Component6 from '../src/components/requests/requests.tsx';
+reactComponents['Requests'] = Component6;
+
+import Component7 from '../src/components/update-request-form/update-request-form.tsx';
+reactComponents['UpdateRequestForm'] = Component7;
